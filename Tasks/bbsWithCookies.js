3b31b0e3db56157a14c09cdc486ef4c41f292f1a
clamp <15528374+DJ-clamp@users.noreply.github.com>
2024-05-21 23:05:01 +0800

    Update bbsWithCookies.js

--- Tasks/bbsWithCookies.js
+++ Tasks/bbsWithCookies.js
@@ -21,26 +21,34 @@ function signPageUrl(options) {
 async function logBBS() {
   const SignBaseUrl = getBaseUrl(opts);
   const SignPageUrl = signPageUrl(opts);
-  const response = await fetch(SignPageUrl, {
-    "headers": {
-      "accept": "text/plain, */*; q=0.01",
-      "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
-      "priority": "u=1, i",
-      "sec-ch-ua": "\"Chromium\";v=\"124\", \"Google Chrome\";v=\"124\", \"Not-A.Brand\";v=\"99\"",
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": "\"Windows\"",
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-      "x-requested-with": "XMLHttpRequest",
-      "cookie": opts.cookie,
-      "Referer": SignBaseUrl,
-      "Referrer-Policy": "strict-origin-when-cross-origin"
-    },
-    "body": null,
-    "method": "POST"
-  });
-  const result = await response.json();
-  return result
+  try {
+    const response = await fetch(signPageUrl, {
+      headers: {
+        "accept": "text/plain, */*; q=0.01",
+        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+        "priority": "u=1, i",
+        "sec-ch-ua": "\"Chromium\";v=\"124\", \"Google Chrome\";v=\"124\", \"Not-A.Brand\";v=\"99\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-requested-with": "XMLHttpRequest",
+        "cookie": opts.cookie,
+        "Referer": SignBaseUrl,
+        "Referrer-Policy": "strict-origin-when-cross-origin"
+      },
+      method: "POST"
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("An error occurred while logging into the BBS:", error);
+    throw error; // 可以选择抛出错误，以便调用者能够处理
+  }
 }
-logBBS().then(res => console.log(res));
+logBBS().then(res => console.log(res)).catch(err => console.error(err));
