8f7e68987df2b04ff050be41c018df411a78458f
clamp <15528374+DJ-clamp@users.noreply.github.com>
2024-06-05 21:48:53 +0800

    Update bbsWithCookies.js

--- Tasks/bbsWithCookies.js
+++ Tasks/bbsWithCookies.js
@@ -22,21 +22,23 @@ async function logBBS() {
   const SignBaseUrl = getBaseUrl(opts);
   const SignPageUrl = signPageUrl(opts);
   try {
+    const enteryURL = await fetch(SignPageUrl, {
+      headers: {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Cookie": opts.cookie,
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+      },
+      "method": "GET"
+    });
     const response = await fetch(SignPageUrl, {
       headers: {
         "accept": "text/plain, */*; q=0.01",
-        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
-        "priority": "u=1, i",
+        "Accept-Encoding":"gzip, deflate, br, zstd",
         "sec-ch-ua": "\"Chromium\";v=\"124\", \"Google Chrome\";v=\"124\", \"Not-A.Brand\";v=\"99\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"Windows\"",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
         "x-requested-with": "XMLHttpRequest",
-        "cookie": opts.cookie,
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+        "Cookie": opts.cookie,
         "Referer": SignBaseUrl,
-        "Referrer-Policy": "strict-origin-when-cross-origin"
       },
       method: "POST"
     });
